249f83ba9adda258dd8ce8e906a8285c5c658cdf
BowieRox <131228028+BowieRox@users.noreply.github.com>
2023-04-19 00:19:14 -0400

    Add files via upload

---
+++ candle-with-label.php
@@ -0,0 +1,21 @@
+<?php
+// Get the label text from the URL query parameters
+$labelText = isset($_GET['label']) ? $_GET['label'] : '';
+
+// Load the candle image
+$candleImage = imagecreatefromjpeg('candle.jpg');
+
+// Set up the font and label text color
+$font = 'poppins-medium.ttf';
+$labelTextColor = imagecolorallocate($candleImage, 255, 255, 255);
+
+// Add the label text to the image
+imagettftext($candleImage, 40, 0, 200, 300, $labelTextColor, $font, $labelText);
+
+// Output the image to the browser
+header('Content-Type: image/jpeg');
+imagejpeg($candleImage);
+
+// Clean up
+imagedestroy($candleImage);
+?>
